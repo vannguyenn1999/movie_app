81f57825f427cb9fc1e54339d1dfaa5e894fe524
vannguyenn1999 <73245636+vannguyenn1999@users.noreply.github.com>
2025-05-17 09:20:40 +0700

    fix loading 2

--- src/layouts/loading/Loading2.tsx
+++ src/layouts/loading/Loading2.tsx
@@ -1,13 +1,9 @@
 const LoadingCompoment = () => {
 
   return (
-    <div className="w-full flex justify-center items-center">
-        <div className="relative w-12 h-12 rounded-full animate-spin">
-        <div className="absolute top-0 left-0 w-full h-full border-t-4 border-blue-500 rounded-full"></div>
-        <div className="absolute top-0 left-0 w-full h-full border-r-4 border-green-500 rounded-full"></div>
-        <div className="absolute top-0 left-0 w-full h-full border-b-4 border-yellow-500 rounded-full"></div>
-        </div>
-    </div>
+  <div className="w-full flex justify-center items-center">
+    <div className="w-10 h-10 border-4 border-blue-500 border-t-red-500 border-r-green-500 border-b-yellow-500 rounded-full animate-spin"></div>
+  </div>
   );
 };
 
